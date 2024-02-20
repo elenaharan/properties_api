3e307d5d115922eae422f67f4ec5f6e9e78453b1
Elena Haran <elenaharan@gmail.com>
2024-02-20 15:04:46 +0000

    Implement get method

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Models = require("./models");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+const Properties = Models.Property;
+
+mongoose.connect("mongodb://localhost:27017/properties");
+
+app.get("/properties", async (req, res) => {
+  await Properties.find()
+    .then((properties) => {
+      res.status(201).json(properties);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Your app is listening on port 3000.");
+});
